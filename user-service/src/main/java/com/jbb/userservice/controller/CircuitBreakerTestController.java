cd6065306f6058488b48f1d29e07924c4ed921a0
jbb9229 <jbb9229@gmail.com>
2023-06-11 23:05:26 +0900

    REFACTOR: gateway-service Connect user-service
    request url 변경
    sample http request 추가

--- user-service/src/main/java/com/jbb/userservice/controller/CircuitBreakerTestController.java
+++ user-service/src/main/java/com/jbb/userservice/controller/CircuitBreakerTestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/circuit-breaker")
+@RequestMapping("/user/circuit-breaker")
 public class CircuitBreakerTestController {
 
     private final CircuitBreakerTestService circuitBreakerTestService;
